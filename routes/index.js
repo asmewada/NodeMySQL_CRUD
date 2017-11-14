7ed10682f31c2a62df3c353113aa2ff867b5aa82
future <future@ubuntu.ubuntu-domain>
2017-11-14 21:44:15 +0530

    first commit

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'NodeJS MySQL CRUD' });
+});
+
+router.get('/', function(req, res, next){
+	
+});
+
+router.post('/', function(req, res, next){
+
+});
+
+router.post('/', function(req, res, next){
+
+});
+
+router.post('/', function(req, res, next){
+
+});
+module.exports = router;
